0fc57a4b2a66face5038db140653f0f3296efda7
munirajareddy23 <bmuniraja.res23@gmail.com>
2019-11-28 22:41:53 +0530

    change in print stmt

--- SpringBootDemo/src/main/java/com/muniraja/demo/hello/TestController.java
+++ SpringBootDemo/src/main/java/com/muniraja/demo/hello/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 	@RequestMapping("/hello")
 	public String display() {
-		return "Springboot demo message";
+		return "Springboot demo msg";
 	}
 
 }
